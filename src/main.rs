e27c5b3d8fd4e6fe9d303c3fa96f1d0d0e553dae
Henri Chataing <chataing.henri@gmail.com>
2019-08-05 23:35:36 +0200

    Basic mouse control

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use io::off::import;
 use std::fs::File;
 
 use sfml::graphics::{RenderWindow, RenderTarget};
-use sfml::window::{Event, Key, Style};
+use sfml::window::{Event, Key, mouse::Button, Style};
 use sfml::system::Vector2i;
 
 use renderer::*;
@@ -25,7 +25,7 @@ use std::f32;
 fn main() {
 
     let mut mesh = Mesh::new();
-    let mesh_file = "objects/cube.off";
+    let mesh_file = "objects/space_station.off";
     let mut file = File::open(mesh_file).unwrap();
 
     let res = import(&mut file, &mut mesh);
@@ -34,40 +34,42 @@ fn main() {
         return;
     }
 
+    let width: u32 = 800;
+    let height: u32 = 600;
+
     // Rotate the mesh and translate it
-    mesh.translate(Vector3::new(0.,0.,-2.));
+    mesh.translate(Vector3::new(0.,0.,-12.));
     mesh.rot_x(-f32::consts::PI/2.);
 
     let mut camera = Camera::new();
     let mut window = RenderWindow::new(
-        (800,600),
+        (width, height),
         "Dot",
         Style::CLOSE,
         &Default::default(),
     );
     window.set_vertical_sync_enabled(true);
     window.set_framerate_limit(60);
+    window.set_mouse_position(&Vector2i::new(width as i32 / 2, height as i32 / 2));
 
     // Create empty Z-buffer
-    let mut zbuf = vec![std::f32::MIN; (window.size().x * window.size().y) as usize];
-
-    let mut mx = 400;
-    let mut my = 300;
-
-    window.set_mouse_position(&Vector2i::new(400,300));
-
-    println!("{} =? {}", f32::consts::PI/4., f32::atan(1.));
+    let mut zbuf = vec![std::f32::MIN; (width * height) as usize];
 
     let mut paused = false;
     let mut mouse_left = false;
+    let mut start_coords = None;
 
     loop {
         while let Some(event) = window.poll_event() {
             match event {
-                Event::Closed | Event::KeyPressed { code : Key::Escape, .. }
-                    => return,
+                Event::Closed | Event::KeyPressed { code: Key::Escape, .. } =>
+                    return,
                 Event::KeyPressed { code: Key::Space, .. } =>
                     paused = !paused,
+                Event::MouseButtonPressed { button: Button::Left, x, y } =>
+                    start_coords = Some ((x as f32, y as f32)),
+                Event::MouseButtonReleased { button: Button::Left, .. } =>
+                    start_coords = None,
                 _ => {}
             }
         }
@@ -76,22 +78,21 @@ fn main() {
             continue
         }
 
-        let mp = window.mouse_position();
-        let dx = (mp.x - mx) as f32;
-        let dy = (mp.y - my) as f32;
-        mx = mp.x;
-        my = mp.y;
+        match start_coords {
+            Some ((sx, sy)) => {
+                let mp = window.mouse_position();
+                mesh.rot_y((mp.x as f32 - sx) * 0.001);
+                mesh.rot_x((mp.y as f32 - sy) * 0.001);
+            },
+            _ => {}
+        }
 
-        //camera.rot_z(dx/20.);
-        //camera.rot_x(dy/20.);
-
-        // Clear the Z-buffer
+        // Clear the Z-buffer.
         for c in zbuf.iter_mut() {
             *c = std::f32::MAX;
         }
 
         render_raster(&mut window, &mut zbuf, &mesh, &camera);
         window.display();
-        mesh.rot_y(f32::consts::PI/180.)
     }
 }
